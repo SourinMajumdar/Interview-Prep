eb2dc15b393ab5748faeb57bdfe3e7c9a7206f83
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-05-02 20:35:19 +0530

    lc solution

---
+++ java/5-arrays-&-arraylist/Problems/BuildArrayFromPermutation.java
@@ -0,0 +1,31 @@
+/*
+LEETCODE:
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+Example 1:
+
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+
+Example 2:
+
+Input: nums = [5,0,1,2,3,4]
+Output: [4,5,0,1,2,3]
+
+https://leetcode.com/problems/build-array-from-permutation/
+
+ */
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        int i=0;
+        while(i<nums.length){
+            ans[i]=nums[nums[i]];
+            ++i;
+        }
+        return ans;
+    }
+}
+
