44c7fdcd12f258e62b9d1746c5cfb9f4b1801803
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-06-10 11:55:27 +0530

    ...

--- java/6-searching/Problems/SmallestLetter.java
+++ java/6-searching/Problems/SmallestLetter.java
@@ -34,7 +34,7 @@ class Solution {
         while(start<=end){
             int mid = start +(end-start)/2;
             if(target<letters[mid]) end = mid-1;
-            else start = mid+1;
+            else start = mid+1; // target>letters[mid]
         }
         return letters[start % letters.length];
 
