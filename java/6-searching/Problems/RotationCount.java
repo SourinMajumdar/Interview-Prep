946289d7d9b6be0b6db3b7bba51b9659c47d3687
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-06-11 11:36:41 +0530

    ...

--- java/6-searching/Problems/RotationCount.java
+++ java/6-searching/Problems/RotationCount.java
@@ -23,4 +23,28 @@ public class RotationCount {
         }
         return -1;
     }
+
+    static int findPivotWithDuplicates(int[] arr){
+        int start = 0, end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]) return mid;
+            if(mid>start && arr[mid]<arr[mid-1]) return mid-1;
+
+            // if elements at start, mid, end are equal, then just skip the duplicates
+            if(arr[start]==arr[mid] && arr[end]==arr[mid]){
+                // check if start is pivot
+                if(arr[start]>arr[start+1]) return start;
+                start++;
+
+                //check if end is pivot
+                if(arr[end]<arr[end-1]) return end-1;
+                end--;
+            }
+            // left side has bigger elements, min will be in the right eg.: [3,4,5,1,2]
+            else if(arr[start]<=arr[mid] && arr[mid]>arr[end]) start=mid+1;
+            else end = mid-1;
+        }
+        return -1;
+    }
 }
