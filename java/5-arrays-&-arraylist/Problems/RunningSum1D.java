5726b5d8071a91e8a579cc3d55c0c7508967db5c
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-07-25 23:03:48 +0530

    new solution

--- java/5-arrays-&-arraylist/Problems/RunningSum1D.java
+++ java/5-arrays-&-arraylist/Problems/RunningSum1D.java
@@ -23,13 +23,9 @@ Problem link: https://leetcode.com/problems/running-sum-of-1d-array/
 
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] runningSum = new int[nums.length];
-        int sum = 0, i=0;
-        while(i<nums.length){
-            sum += nums[i];
-            runningSum[i]=sum;
-            ++i;
+        for(int i = 1; i<nums.length; ++i){
+            nums[i] += nums[i-1];
         }
-        return runningSum;
+        return nums;
     }
 }
