02cfaed15a3b68ad3932821bf5038c3a99e8928e
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-08-05 23:41:14 +0530

    4 August 2022

---
+++ java/5-arrays-&-arraylist/Problems/SortColors.java
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/sort-colors/
+// Solution: https://www.youtube.com/watch?v=uvB-Ns_TVis
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums.length == 1) return;
+
+        int  start = 0, end = nums.length - 1, i = 0;
+
+        while( start < end && i <= end ) {
+            if (nums[i] == 0) {
+                nums[i] = nums[start];
+                nums[start] = 0;
+                i++; start++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[end];
+                nums[end] = 2;
+                end--;
+            } else {
+                i++;
+            }
+        }
+    }
+}
+
+// TC: O(n), SC: O(1)
