64bca19d8559b242d69a0dbfb14fcd2117135581
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-08-08 17:37:33 +0530

    8 July 2022

--- java/5-arrays-&-arraylist/Problems/ThreeSum.java
+++ java/5-arrays-&-arraylist/Problems/ThreeSum.java
@@ -34,5 +34,35 @@ class Solution {
 // m --> number of unique triplets
 
 
+// Optimised solution:
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans =  new LinkedList<>();
 
-//
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lo = i + 1, hi = nums.length - 1, sum = -nums[i];
+
+                while ( lo < hi ) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+
+                        while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
+
+                        lo++; hi--;
+                    }
+                    else if (nums[lo] + nums[hi] < sum) lo++;
+                    else hi--;
+
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+// TC: O(n^2), SC: O(M)
+// Auxiliary space: O(1)
+// M --> Number of unique triplets
