dac83d09f108ee2c8b1481dfe70dadbdfb23e074
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-08-10 22:07:46 +0530

    9 August 2022

--- java/5-arrays-&-arraylist/Problems/MedianOfTwoSortedArrays.java
+++ java/5-arrays-&-arraylist/Problems/MedianOfTwoSortedArrays.java
@@ -1,5 +1,5 @@
 // Problem: https://leetcode.com/problems/median-of-two-sorted-arrays/
-// Solution:
+// Solution: https://www.youtube.com/watch?v=NTop3VTjmxk
 
 
 // Prerequisite: Merge two sorted arrays
@@ -57,6 +57,46 @@ class Solution {
 }
 
 // TC: O(m + n), SC: O(m + n)
+
+// Using binary search
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
+
+        int lo = 0, hi = m;
+
+        while (lo <= hi) {
+            int cut1 = (lo + hi) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // Partition found
+            if ( l1 <= r2 && l2 <= r1 ) {
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+
+                return Math.max(l1, l2);
+            }
+
+            else if (l1 > r2) hi = cut1 - 1;
+            else lo = cut1 + 1;
+
+        }
+
+        return -1;
+    }
+}
+
+// TC: O(log(m + n)), SC: O(1)
 
 
 
