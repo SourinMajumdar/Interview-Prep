bf41e261cb911b02e5c120013f92a44a383b779a
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-08-29 14:34:10 +0530

    29 August 2022

---
+++ java/5-arrays-&-arraylist/NumberOfIslands.java
@@ -0,0 +1,33 @@
+// Link: https://leetcode.com/problems/number-of-islands/
+// Solution: https://www.youtube.com/watch?v=o8S2bO3pmO4
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands += dfs(grid, i, j);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    public int dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return 0;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        return 1;
+    }
+}
+
+// TC: O(m * n), SC: O(1)
