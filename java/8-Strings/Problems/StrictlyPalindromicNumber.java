598d094e9058dd0b3ac0cbe3a1eb2f29c7db5977
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-09-04 02:34:30 +0530

    3 September 2022

--- java/8-Strings/Problems/StrictlyPalindromicNumber.java
+++ java/8-Strings/Problems/StrictlyPalindromicNumber.java
@@ -6,47 +6,70 @@ class Solution {
     public boolean isStrictlyPalindromic(int n) {
         boolean[] arr = new boolean[n - 3];
 
-        for (int i = 2; i <= n - 2; i++) {  // n
-            String base = baseConverter(n, i);  // n
-            arr[i - 2] = isPalindrome(base);    // n
+        for (int i = 2; i < n - 1; i++) {
+            String base = baseConverter(n, i);
+            arr[i - 2] = isPalindrome(base);
         }
 
-        for (boolean bool : arr) {  // n
+        for (boolean bool : arr) {
             if (!bool) return false;
         }
 
         return true;
     }
 
-    public String baseConverter(int n, int base) {  // Overall O(n)
+    public String baseConverter(int n, int base) {
         String ans = "";
-        while (n > 0) {     // n
+        while (n > 0) {
             int rem = n % base;
             ans += String.valueOf(rem);
             n /= base;
         }
 
         String rev = "";
-        for (int i = ans.length() - 1; i >= 0; i--) {   // n
+        for (int i = ans.length() - 1; i >= 0; i--) {
             rev += ans.charAt(i);
         }
 
         return rev;
     }
 
-    public boolean isPalindrome(String x) {     // Overall O(n)
-        String reverse = "";
-
-        for (int i = x.length() - 1; i >= 0; i--) {     // n
-            reverse += x.charAt(i);
+    public boolean isPalindrome(String s) { // n / 2 => n
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++; j--;
         }
 
-        return x.equals(reverse);   // O(n)
+        return true;
     }
 }
 
 // TC: O(n ^ 2) + O(n) => O(n ^ 2)
 // SC: O(n)
+
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        for (int i = 2; i < n - 1; i++) {
+            String s = Integer.toString(n, i);  // returns ith base of argument n
+            if (!isPalindrome(s)) return false;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++; j--;
+        }
+
+        return true;
+    }
+}
+
+// TC: O(n ^ 2), O(1)
 
 // OR
 
