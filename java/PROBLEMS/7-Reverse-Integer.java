7429124dd931936c8fc3c9227469a1cf7cc143c0
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-09-09 21:44:04 +0530

    9 September 2022

---
+++ java/PROBLEMS/7-Reverse-Integer.java
@@ -0,0 +1,38 @@
+// Link: https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+
+            if (rev < Integer.MIN_VALUE /10 || rev > Integer.MAX_VALUE /10) return 0;
+
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
+
+// TC: O(n), SC: O(1)
+
+class Solution {
+    public int reverse(int x) {
+        boolean isNeg = x < 0;
+        x = Math.abs(x);
+
+        long rev = 0;
+
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+
+        if (rev > Integer.MAX_VALUE) return 0;
+
+        return isNeg ? (int) -rev : (int) rev;
+    }
+}
+
+// TC: O(n), SC: O(1)
