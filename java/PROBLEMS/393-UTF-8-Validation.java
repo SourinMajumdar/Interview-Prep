036cb70dc48a3fea8a9b02b70e4a076cf71dca57
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-09-13 13:44:39 +0530

    13 September 2022

--- java/PROBLEMS/393-UTF-8-Validation.java
+++ java/PROBLEMS/393-UTF-8-Validation.java
@@ -12,26 +12,30 @@
             2          |   110xxxxx 10xxxxxx
             3          |   1110xxxx 10xxxxxx 10xxxxxx
             4          |   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                           |______| |_______|________|________|
+                           1st Byte       Remaining Bytes
 */
 
 class Solution {
     public boolean validUtf8(int[] data) {
-        int remBytes = 0;
+        int remBytes = 0;   // Remaining bytes after the 1st byte
 
         for (int n : data) {
+
             if (remBytes == 0) {
-                if (n >> 7 == 0b0) remBytes = 0;
-                else if (n >> 5 == 0b110) remBytes = 1;
-                else if (n >> 4 == 0b1110) remBytes = 2;
-                else if (n >> 3 == 0b11110) remBytes = 3;
-                else return false;
-            } else {
-                if (n >> 6 == 0b10) remBytes--;
-                else return false;
+                if (n >> 7 == 0b0) remBytes = 0;            // 1st byte of 1 length char (0xxxxxxx)
+                else if (n >> 5 == 0b110) remBytes = 1;     // 1st byte of 2 length char (110xxxxx 10xxxxxx)
+                else if (n >> 4 == 0b1110) remBytes = 2;    // 1st byte of 3 length char (1110xxxx 10xxxxxx 10xxxxxx)
+                else if (n >> 3 == 0b11110) remBytes = 3;   // 1st byte of 4 length char (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
+                else return false;                          // NOTA
+
+            } else {                                        // if more than 1 byte, we check if the rest start with 10
+                if (n >> 6 == 0b10) remBytes--;             // if yes, we decrease remaining bytes
+                else return false;                          // If not, sequence not valid
             }
         }
 
-        return remBytes == 0;
+        return remBytes == 0;       // At the end if there are no remening bytes, sequence is valid.
     }
 }
 
