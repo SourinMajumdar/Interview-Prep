1bf65a3c66ecc03863d8be01fd5bec9504617f6e
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-09-16 15:48:50 +0530

    16 September 2022

--- java/5-arrays-&-arraylist/Problems/SortColors.java
+++ java/5-arrays-&-arraylist/Problems/SortColors.java
@@ -1,26 +1,48 @@
 // Problem: https://leetcode.com/problems/sort-colors/
 // Solution: https://www.youtube.com/watch?v=uvB-Ns_TVis
 
+// Dutch National Flag algorithm
 class Solution {
     public void sortColors(int[] nums) {
-        if (nums.length == 1) return;
+        int  lo = 0, hi = nums.length - 1, mid = 0;
 
-        int  start = 0, end = nums.length - 1, i = 0;
+        while (mid <= hi) {
 
-        while( start < end && i <= end ) {
-            if (nums[i] == 0) {
-                nums[i] = nums[start];
-                nums[start] = 0;
-                i++; start++;
-            } else if (nums[i] == 2) {
-                nums[i] = nums[end];
-                nums[end] = 2;
-                end--;
-            } else {
-                i++;
+            if (nums[mid] == 0) {
+                nums[mid++] = nums[lo];
+                nums[lo++] = 0;
             }
+
+            else if (nums[mid] == 2) {
+                nums[mid] = nums[hi];
+                nums[hi--] = 2;
+            }
+
+            else mid++;
         }
     }
 }
 
 // TC: O(n), SC: O(1)
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0, count1 = 0;
+
+        for (int n : nums) {
+            if (n == 0) count0++;
+            else if (n == 1) count1++;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < count0) nums[i] = 0;
+            else if (i < count0 + count1) nums[i] = 1;
+            else nums[i] = 2;
+        }
+    }
+}
+
+// TC: O(n), SC: O(1)
+
+
