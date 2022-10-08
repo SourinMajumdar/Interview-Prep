9f6fdb0481ff07c333b7d5032a0a075cdbb02338
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-10-08 16:07:55 +0530

    8 October 2022

--- java/PROBLEMS/15-3Sum.java
+++ java/PROBLEMS/15-3Sum.java
@@ -29,9 +29,61 @@ class Solution {
         return ans;
     }
 }
-
 // TC: O(n^3 * logm), SC: O(m)
 // m --> number of unique triplets
+
+// OR
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ans = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    }
+                }
+            }
+        }
+
+        return new ArrayList(ans);
+    }
+}
+
+// TC: O(n * logn) + O(n^3 * logk) => O(n^3 * logk)
+// SC: O(K) - k is the number of unique triplets
+
+
+
+// little optimisation
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ans = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> set = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int newTarget = 0 - nums[i] - nums[j];
+                if (set.contains(newTarget)) {
+                    ans.add(Arrays.asList(nums[i], newTarget, nums[j]));
+                }
+                else set.add(nums[j]);
+            }
+        }
+
+        return new ArrayList(ans);
+    }
+}
+
+// TC: O(n * logn) + O(n^2 * logk) => O(n^2 * logk)
+// SC: O(K) - k is the number of unique triplets
+
 
 
 // Optimised solution:
