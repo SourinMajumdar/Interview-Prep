52ebee1f102e16991bdedaed058f4f04cd6e8825
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-10-09 20:12:38 +0530

    9 October 2022

--- java/PROBLEMS/653-Two-Sum-IV-Input-is-a-BST.java
+++ java/PROBLEMS/653-Two-Sum-IV-Input-is-a-BST.java
@@ -50,4 +50,17 @@ class Solution {
     }
 }
 
+// or
+
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) return false;
+        if (set.contains(k - root.val)) return true;
+        set.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+}
+
+
 // TC: O(n), SC: O(n)
