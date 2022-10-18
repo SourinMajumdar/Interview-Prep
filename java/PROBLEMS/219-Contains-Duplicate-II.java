9826547decfe4989b5a07576bc358bd6435f834d
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-10-18 16:01:09 +0530

    18 October 2022

--- java/PROBLEMS/219-Contains-Duplicate-II.java
+++ java/PROBLEMS/219-Contains-Duplicate-II.java
@@ -1,5 +1,45 @@
 // Link: https://leetcode.com/problems/contains-duplicate-ii/
 
+
+// Brute
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (nums[i] == nums[j] && j - i <= k)
+                    return true;
+
+        return false;
+    }
+}
+
+// TC: O(n ^ 2), SC: O(1)
+
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
+
+// TC: O(n), SC: O(k)
+
+
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
