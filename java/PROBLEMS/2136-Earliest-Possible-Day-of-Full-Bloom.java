1899c13091142503bd4b92e89bd569be8f4207e1
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-10-29 11:51:16 +0530

    29 October 2022

---
+++ java/PROBLEMS/2136-Earliest-Possible-Day-of-Full-Bloom.java
@@ -0,0 +1,39 @@
+// Link: https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
+// https://www.youtube.com/watch?v=aeKAMmZjvb4
+
+
+class Solution {
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        List<Seed> plants = new ArrayList<>();
+        int n = plantTime.length;
+
+        for (int i = 0; i < n; i++) {
+            plants.add(new Seed(plantTime[i], growTime[i]));
+        }
+
+        Collections.sort(plants, (a, b) -> b.growTime - a.growTime);
+
+        int time = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            time += plants.get(i).plantTime;
+            max = Math.max(max, time + plants.get(i).growTime);
+        }
+
+        return max;
+    }
+
+    private class Seed {
+        int plantTime;
+        int growTime;
+
+        public Seed (int p, int g) {
+            this.plantTime = p;
+            this.growTime = g;
+        }
+    }
+}
+
+// TC: O(2 * n) + O(n * logn) => O(n ** logn)
+// SC: O(n)
