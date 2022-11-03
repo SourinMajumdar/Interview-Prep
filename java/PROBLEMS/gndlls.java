6aff52cd51ceb8f31d401e455a2ad465c351664c
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-03 23:59:51 +0530

    3 November 2022

---
+++ java/PROBLEMS/gndlls.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return merge(root1, root2);
+    }
+
+    public TreeNode merge(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 != null && t2 == null) return t1;
+        if (t1 == null && t2 != null) return t2;
+
+        t1.val += t2.val;
+
+        t1.left = merge(t1.left, t2.left);
+        t1.right = merge(t1.right, t2.right);
+
+        return t1;
+    }
+}
