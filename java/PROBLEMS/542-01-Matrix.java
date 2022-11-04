8d703eefae9d4cd1e12b54d46eb8681ce557fb2d
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-04 23:22:59 +0530

    4 November 2022

---
+++ java/PROBLEMS/542-01-Matrix.java
@@ -0,0 +1,47 @@
+// Link: https://leetcode.com/problems/01-matrix/
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+
+        Queue<int[]> queue=new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.add(new int[]{i, j});
+                }
+                else mat[i][j] = -1;
+            }
+        }
+
+        int[][] dirs = {{0,1},{-1,0},{1,0},{0,-1}};
+
+        int length = 0;
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            length++;
+
+            while (size-- > 0){
+                int[] cell = queue.poll();
+
+                for (int[] dir : dirs){
+                    int r = cell[0] + dir[0];
+                    int c = cell[1]  +dir[1];
+
+                    if (r < 0 || c < 0 || r == m || c == n || mat[r][c] != -1) {
+                        continue;
+                    }
+
+                    mat[r][c] = length;
+                    queue.add(new int[]{r, c});
+                }
+            }
+        }
+
+        return mat;
+    }
+}
+
+// TC O(m * n),
