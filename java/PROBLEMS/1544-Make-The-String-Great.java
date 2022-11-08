80fd9cf7cae77800298c83fcd7dda542ca4ae28d
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-08 17:43:10 +0530

    8 November 2022

--- java/PROBLEMS/1544-Make-The-String-Great.java
+++ java/PROBLEMS/1544-Make-The-String-Great.java
@@ -34,3 +34,29 @@ class Solution {
 }
 
 // TC: O(n), SC: O(n)
+
+
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && Math.abs(stack.peek() - s.charAt(i)) == 32) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(s.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
+// TC: O(n), SC: O(n)
