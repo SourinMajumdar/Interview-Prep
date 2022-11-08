6b0b0b4c86f5759a97007cd61547ed0b43b294eb
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-08 15:47:31 +0530

    8 November 2022

---
+++ java/PROBLEMS/1544-Make-The-String-Great.java
@@ -0,0 +1,35 @@
+// Link: https://leetcode.com/problems/make-the-string-great/
+
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char top = '0';
+            if (!stack.isEmpty()) top = stack.peek();
+            char curr = s.charAt(i);
+
+            if (toLower(top) == toLower(curr) &&
+                ((isUpper(top) && isLower(curr)) ||
+                (isLower(top) && isUpper(curr)))) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(s.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public char toLower(char c)    {return Character.toLowerCase(c);}
+    public boolean isLower(char c) {return Character.isLowerCase(c);}
+    public boolean isUpper(char c) {return Character.isUpperCase(c);}
+}
+
