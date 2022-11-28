73a6cbe7a3b8e71b4fcc42d19c653f1401341978
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-29 01:59:32 +0530

    28 November 2022

--- java/PROBLEMS/2104-Sum-of-Subarray-Ranges.java
+++ java/PROBLEMS/2104-Sum-of-Subarray-Ranges.java
@@ -29,17 +29,15 @@ class Solution {
 // AC
 class Solution {
     public long subArrayRanges(int[] nums) {
+        int n = nums.length;
         long sum = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
-            min = max = nums[i];
-            sum += (max - min);
+        for (int i = 0; i < n; i++) {
+            int max = nums[i], min = nums[i];
 
-            for (int j = i + 1; j < nums.length; j++) {
-                min = Math.min(min, nums[j]);
+            for (int j = i + 1; j < n; j++) {
                 max = Math.max(max, nums[j]);
+                min = Math.min(min, nums[j]);
                 sum += (max - min);
             }
         }
