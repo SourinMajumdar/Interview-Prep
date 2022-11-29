5279051a245e4e1d39a2059112df920d4d629e2c
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-11-30 01:55:56 +0530

    29 November 2022

---
+++ java/PROBLEMS/954-Array-of-Doubled-Pairs.java
@@ -0,0 +1,29 @@
+// Link: https://leetcode.com/problems/array-of-doubled-pairs/
+
+class Solution {
+    public boolean canReorderDoubled(int[] arr) {
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int n : arr) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (int n : map.keySet()) {
+            if (map.get(n) > 0) {
+                if (n < 0 && n % 2 != 0) return false;
+
+                int target = n < 0 ? n / 2 : n * 2;
+
+                if (map.get(n) > map.getOrDefault(target, 0)) {
+                    return false;
+                }
+
+                map.put(target, map.get(target) - map.get(n));
+            }
+        }
+
+        return true;
+    }
+}
+
+// TC: O(n * logn), SC: O(n)
