b5e8ccd98f3bca8a4b92fcc419554a5e48c4e65a
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-12-06 12:35:10 +0530

    6 December 2022

---
+++ java/PROBLEMS/328-Odd-Even-Linked-List.java
@@ -0,0 +1,89 @@
+// Link: https://leetcode.com/problems/odd-even-linked-list/
+
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = new ListNode(-1);
+        ListNode even = new ListNode(-1);
+
+        ListNode oddHead = odd, evenHead = even;
+
+        int i = 1;
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (i % 2 == 1) {
+                oddHead.next = temp;
+                oddHead = oddHead.next;
+            }
+            else {
+                evenHead.next = temp;
+                evenHead = evenHead.next;
+            }
+
+            temp = temp.next;
+            i++;
+        }
+
+        oddHead.next = null; evenHead.next = null;
+
+        ListNode ans = new ListNode(-1);
+        ListNode ansHead = ans;
+        temp = odd.next;
+
+        while (temp != null) {
+            ansHead.next = temp;
+            ansHead = ansHead.next;
+            temp = temp.next;
+        }
+
+        ansHead.next = even.next;
+
+        return ans.next;
+    }
+}
+
+// TC: O(n), SC: O(1)
+
+
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+
+        ListNode odd = head,
+        even = head.next,
+        temp = head.next.next;
+
+        odd.next = null; even.next = null;
+
+        ListNode prev = null,
+        evenHead = even,
+        oddHead = odd;
+
+        int i = 1;
+        while (temp != null) {
+            prev = temp;
+
+            if (i % 2 == 1) {
+                oddHead.next = temp;
+                oddHead = oddHead.next;
+            }
+            else {
+                evenHead.next = temp;
+                evenHead = evenHead.next;
+            }
+
+            temp = temp.next;
+            prev.next = null;
+            i++;
+        }
+
+        oddHead.next = even;
+        return odd;
+    }
+}
+
+// TC: O(n), SC: O(1)
