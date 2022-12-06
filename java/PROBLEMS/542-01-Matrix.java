25ed38485fd26c03d06e7540b623d71c847f33c7
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-12-06 23:13:03 +0530

    6 December 2022

--- java/PROBLEMS/542-01-Matrix.java
+++ java/PROBLEMS/542-01-Matrix.java
@@ -44,3 +44,51 @@ class Solution {
 }
 
 // TC O(m * n), SC: O(m * n)
+
+
+// GFG: https://practice.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
+class Solution {
+    public int[][] nearest(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        int[][] ans = new int[m][n];
+        int distance = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0){
+                int[] cell = queue.poll();
+                int x = cell[0], y = cell[1];
+                ans[x][y] = distance;
+
+                for (int[] dir : dirs){
+                    int r = x + dir[0];
+                    int c = y + dir[1];
+
+                    if (r >= 0 && c >= 0 && r < m && c < n && !visited[r][c]) {
+                        visited[r][c] = true;
+                        queue.add(new int[]{r, c});
+                    }
+                }
+            }
+
+            distance++;
+        }
+
+        return ans;
+    }
+}
+
