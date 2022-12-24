84fc49bb83c155521bdd941794b551525ef84a85
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-12-25 02:39:45 +0530

    24 December 2022

--- java/PROBLEMS/2512-Reward-Top-K-Students.java
+++ java/PROBLEMS/2512-Reward-Top-K-Students.java
@@ -57,3 +57,48 @@ class Solution {
 // f -> size of feedback arrays
 // n -> size report array
 // r -> size of a word in report array
+
+
+
+class Solution {
+    class student {
+        int id;
+        int pts;
+        public student(int id, int pts) {
+            this.id = id;
+            this.pts = pts;
+        }
+    }
+
+    public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback, String[] report, int[] student_id, int k) {
+        Set<String> posFeedback = new HashSet<>(Arrays.asList(positive_feedback));
+        Set<String> negFeedback = new HashSet<>(Arrays.asList(negative_feedback));
+
+        List<student> list = new ArrayList<>();
+        int idx = 0;
+
+        for (String r : report) {
+            int pts = 0;
+            for (String w : r.split(" ")) {
+                if (posFeedback.contains(w)) pts += 3;
+                if (negFeedback.contains(w)) pts--;
+            }
+
+            list.add(new student(student_id[idx++], pts));
+        }
+
+        Collections.sort(list,
+                (a, b) -> a.pts == b.pts? a.id - b.id : b.pts - a.pts
+        );
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            ans.add(list.get(i).id);
+        }
+
+        return ans;
+    }
+}
+
+// TC: O(f) + O(n * r) + O(n * logn)
+// SC: O(f + n + k)
