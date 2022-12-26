e1befa4530d9d7af8c9e3f7c6169c7285c6a4281
Sourin Majumdar <sourinkrillex37@gmail.com>
2022-12-26 18:49:16 +0530

    26 December 2022

--- java/PROBLEMS/287-Find-the-Duplicate-Number.java
+++ java/PROBLEMS/287-Find-the-Duplicate-Number.java
@@ -16,7 +16,8 @@ class Solution {
     }
 }
 
-// TC: O(nlogn) + O(n), SC: O(1)
+// TC: O(nlogn) + O(n) ~ O(n * logn)
+// SC: O(1)
 
 
 class Solution {
@@ -47,13 +48,11 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int index = Math.abs(nums[i]) - 1;
-
             if (nums[index] < 0) {
                 ans = Math.abs(nums[i]);
                 break;
             }
-
-            nums[index] = - nums[index];
+            nums[index] = -nums[index];
         }
 
         return ans;
@@ -67,22 +66,74 @@ class Solution {
 
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0], fast = nums[0];
+        int slow = nums[0];
+        int fast = nums[0];
 
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-
         } while (slow != fast);
 
         fast = nums[0];
-
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
 
         return slow;
+    }
+}
+
+// TC: O(n), SC: O(1)
+
+
+// Cycle sort
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIdx = nums[i] - 1;
+            if (nums[i] != nums[correctIdx]) {
+                int temp = nums[i];
+                nums[i] = nums[correctIdx];
+                nums[correctIdx] = temp;
+            }
+            else i++;
+        }
+
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return nums[i];
+            }
+        }
+
+        return 0;
+    }
+}
+
+// TC: O(n) + O(n) ~ O(n)
+// SC: O(1)
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int correctIdx = nums[i] - 1;
+                if (nums[i] != nums[correctIdx]) {
+                    int temp = nums[i];
+                    nums[i] = nums[correctIdx];
+                    nums[correctIdx] = temp;
+                }
+                else {
+                    return nums[i];
+                }
+            }
+            else i++;
+
+        }
+
+        return 0;
     }
 }
 
