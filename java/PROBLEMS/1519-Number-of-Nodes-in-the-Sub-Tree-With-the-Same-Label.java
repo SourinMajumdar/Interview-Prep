1a278238edb08479037962d5a30cb1f2fc40fbd7
Sourin Majumdar <sourinkrillex37@gmail.com>
2023-01-12 16:44:16 +0530

    12 January 2023

--- java/PROBLEMS/1519-Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label.java
+++ java/PROBLEMS/1519-Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label.java
@@ -5,42 +5,45 @@ class Solution {
     private int[] ans;
     private List<Integer>[] adj;
     public int[] countSubTrees(int n, int[][] edges, String labels) {
-        adj = new List[n];               // initialising the adjacency list
+        adj = new List[n];     // initialising the adjacency list
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
         }
-        for (int[] e : edges) {          // populating the adjacency list
+        for (int[] e : edges) {     // populating the adjacency list
             adj[e[0]].add(e[1]);
             adj[e[1]].add(e[0]);
         }
 
-        ans = new int[n];                // initialising the answer array
+        ans = new int[n];      // initialising the answer array
         dfs(0, -1, labels);    // calling the dfs
         return ans;
     }
 
-    private int[] dfs(int curr, int parent, String labels) {
-        // Our DFS will go to the depth and bring us frequencies of characters present present in subtrees, from bottom to top
-        int[] currFreq = new int[26];   // records the frequency of all characters in the subtree
-        for (int child : adj[curr]) {   // we look at all the nodes connected to our current node
-            if (child == parent) {      // we don't want to go back to parent node where we came from
+    private int[] dfs(int currNode, int parent, String labels) {
+        // Our DFS will go to the depth and bring us frequencies of characters present in each subtree,
+        // go to bottom and come back to top with frequencies
+        int[] currSubtreeFreq = new int[26];    // records the frequency of all characters in the current node's subtree
+        for (int child : adj[currNode]) {       // we look at all the nodes connected to our current node
+            // we don't want to go back to parent node where we came from
+            if (child == parent) {  // so keep iterating if child is equal to parent
                 continue;
             }
-            // we now call dfs to record the frequency of all chacaters present below the child
-            int[] freqBelow = dfs(child, curr, labels);     // new current is the child, new parent is our current
-            // we update our currFreq array after we get freqBelow array populated
+            // we now call dfs to record the frequencies of all chacaters present in the subtree of the child
+            int[] childSubtreeFreq = dfs(child, currNode, labels);     // child becomes new the currentNode, currentNode becomes the new parent
+            // we update our currSubtreeFreq array after we get childSubtreeFreq array populated
+            // because child's subtree is a part of our current node's subtree
             for (int i = 0; i < 26; i++) {
-                currFreq[i] += freqBelow[i];
+                currSubtreeFreq[i] += childSubtreeFreq[i];
             }
         }
-        // the current node is also associated to a character so we update currFreq
-        currFreq[labels.charAt(curr) - 'a']++;
-        // now, currFreq contains the frequency of all characters present in subtree.
+        // the current node is also associated to a character so we update currSubtreeFreq
+        currSubtreeFreq[labels.charAt(currNode) - 'a']++;
+        // now, currSubtreeFreq contains the frequency of all characters present in subtree.
         // but now we just need the frequency of that particular character which is associated to our current node
         // say current node value is 1 and the letter associated is 'a'.
-        // so we need the frequency of 'a's that have been found in the subtree
-        ans[curr] = currFreq[labels.charAt(curr) - 'a'];
-        return currFreq;    // we return the subtree frequency array which will be utilised upon backtracking
+        // so we need the frequency of 'a's that are in the subtree
+        ans[currNode] = currSubtreeFreq[labels.charAt(currNode) - 'a'];
+        return currSubtreeFreq;    // we return the current subtree frequency array which will be utilised upon backtracking
     }
 }
 
