92c04ee6ffd9e6fea3d3b9c0f5d3398b87672d7e
Sourin Majumdar <sourinkrillex37@gmail.com>
2023-01-30 02:30:22 +0530

    29 January 2023

---
+++ java/PROBLEMS/461-Hamming-Distance.java
@@ -0,0 +1,46 @@
+// Link: https://leetcode.com/problems/hamming-distance/
+
+// x & 1 will give us the last bit of x
+// y & 1 will give us the last bit of y
+
+// If they are unequal, increment count
+// Right shift x and y
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        while (x > 0 || y > 0) {
+            if ((x & 1) != (y & 1)) {
+                count++;
+            }
+            y >>= 1;
+            x >>= 1;
+        }
+
+        return count;
+    }
+}
+
+// XOR of them will give 1, if they are unequal
+// otherwise it'll give 0
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        while (x > 0 || y > 0) {
+            count += (x & 1) ^ (y & 1);
+            y >>= 1;
+            x >>= 1;
+        }
+
+        return count;
+    }
+}
+
+// TC: O(32) => O(1)
+// SC: O(1)
+
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
