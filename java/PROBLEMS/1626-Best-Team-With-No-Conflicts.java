69e99c06900387e8313c2cf480c0cf3213a66f82
Sourin Majumdar <sourinkrillex37@gmail.com>
2023-01-31 19:30:37 +0530

    31 January 2023

---
+++ java/PROBLEMS/1626-Best-Team-With-No-Conflicts.java
@@ -0,0 +1,38 @@
+// Link: https://leetcode.com/problems/best-team-with-no-conflicts/description/
+
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = scores.length;
+        if (n == 1) {
+            return scores[0];
+        }
+
+        int[][] team = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            team[i][0] = ages[i];
+            team[i][1] = scores[i];
+        }
+
+        Arrays.sort(team,
+            (a, b) -> (a[0] == b[0])? a[1] - b[1] : a[0] - b[0]
+        );
+
+        int maxScore = 0;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = team[i][1];
+            for (int j = 0; j < i; j++) {
+                if (team[i][1] >= team[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + team[i][1]);
+                }
+                maxScore = Math.max(dp[i], maxScore);
+            }
+        }
+
+        return maxScore;
+    }
+}
+
+// TC: O(n) + O(n * logn) + O(n^2)
+// SC: O(3n) => O(n)
