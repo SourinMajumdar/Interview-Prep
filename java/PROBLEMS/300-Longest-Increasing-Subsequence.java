f83372e74a9e8dcad12f96fd36c8ef05f734bb99
Sourin Majumdar <sourinkrillex37@gmail.com>
2023-01-31 22:57:59 +0530

    31 January 2023

--- java/PROBLEMS/300-Longest-Increasing-Subsequence.java
+++ java/PROBLEMS/300-Longest-Increasing-Subsequence.java
@@ -21,3 +21,50 @@ class Solution {
 
 // TC: O(n * logn) -> Addition or removal from bst takes logn time.
 // SC: O(n)
+
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        // dp array will store the max length of increasing subsequences till each index
+        int[] dp = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;    // the current position is itself a subsequence of length 1
+            // check from starting index to current inex, if we can make a longer subsequence
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {    // if we find an increasing subsequence
+                    dp[i] = Math.max(dp[i], 1 + dp[j]); // update max length found till index i
+                }
+            }
+        }
+
+        int maxLen = 1;     // Array size is atleast 1
+        // iterate over dp array to get the LIS length
+        for (int size : dp) {
+            maxLen = Math.max(size, maxLen);
+        }
+
+        return maxLen;      // return the answer
+    }
+}
+
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int maxLen = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            maxLen = Math.max(dp[i], maxLen);
+        }
+
+        return maxLen;
+    }
+}
